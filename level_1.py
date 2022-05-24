cc4df7332c7d90a4654dedc64bc598f87b8f70be
Faustino Correa <faustino.correa.m@gmail.com>
2022-05-24 15:05:42 -0400

    docstring level 1

--- level_1.py
+++ level_1.py
@@ -1,5 +1,17 @@
 def main():
-    pass
+    """
+    Level 1:
+        Solve http://www.pythonchallenge.com/pc/def/map.html
+            everybody thinks twice before solving this.
+        Hint:
+            - Use the hints. They are helpful, most of the times.
+            - Investigate the data given to you.
+            - Avoid looking for spoilers.
+    """
+    URL = "http://www.pythonchallenge.com/pc/def/map.html"
+    MESSAGE = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
+
+
 
 if __name__ == '__main__':
     main()
