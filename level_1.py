9cc4e31e9769332bb19d0ab1e45fa89d1350c53f
Faustino Correa <faustino.correa.m@gmail.com>
2022-05-24 14:50:37 -0400

    Create level_1.py

---
+++ level_1.py
@@ -0,0 +1,14 @@
+def main():
+    """
+    Resolver http://www.pythonchallenge.com/pc/def/0.html
+    Hint: try to change the URL address.
+    """
+    url = "http://www.pythonchallenge.com/pc/def/0.html"
+    number = 2**38
+
+    url.replace('0', str(number))
+
+    print(url)
+
+if __name__ == "__main__":
+    main()
