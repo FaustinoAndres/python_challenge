90610a36bd4a35e0a44c594c7ac8ab03873f77ae
Faustino Correa <faustino.correa.m@gmail.com>
2022-05-24 15:23:57 -0400

    Update level_1.py

--- level_1.py
+++ level_1.py
@@ -8,8 +8,21 @@ def main():
             - Investigate the data given to you.
             - Avoid looking for spoilers.
     """
-    URL = "http://www.pythonchallenge.com/pc/def/map.html"
+
+    url = "http://www.pythonchallenge.com/pc/def/map.html"
+
     MESSAGE = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
+
+    message_modified = MESSAGE.replace(" ", "+")
+    message_list = list(message_modified)
+
+    message_unencoded_list = list()
+    for char in message_list:
+            message_unencoded_list.append(chr(ord(char) + 2))
+
+    message_unencoded = "".join(message_unencoded_list).replace("-", " ")
+
+    print(message_unencoded)
 
 
 
