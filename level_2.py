70660c300b1e5a6d50a55ebc69b8d1b8132fddf7
Faustino Correa <faustino.correa.m@gmail.com>
2022-05-24 22:17:21 -0400

    load file level 2

--- level_2.py
+++ level_2.py
@@ -1,5 +1,16 @@
-def main():
+import requests
 
+
+def main():
+    """
+    Level 2:
+        Recognize the characters. maybe they are in the book, but MAYBE they are in the page source.
+        Hint:
+            - Use the hints. They are helpful, most of the times.
+            - Investigate the data given to you.
+            - Avoid looking for spoilers.
+
+    """
     pass
 
 
